f2f26e0d3944956b1553817426591e3d57740ac4
HuginRonin <jbelio@edu.tecnocampus.cat>
2020-02-19 00:05:44 +0100

    Implementando Bouncer

---
+++ Assets/Bouncer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncer : MonoBehaviour
+{
+    public float bounceForce;
+    public float angle;
+    Vector3 direction => Quaternion.AngleAxis(angle, Vector3.forward) * transform.right;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        var reciever = other.GetComponent<ExternalForces>();
+        if (reciever)
+        {
+            Debug.Log("si");
+            var force = bounceForce * direction;
+            reciever.Bounce(this, force);
+            //o
+            //other.GetComponent<RigidBody2D>.AddForce(force, ForceMode2D.Impulse); //asegurarse de que esté todo
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, direction * bounceForce);
+    }
+}
---
+++ Assets/ExternalForces.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalForces : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Bounce(Bouncer b, Vector2 force)
+    {
+        
+        gameObject.GetComponent<Rigidbody2D>().AddForce(force, ForceMode2D.Impulse);
+    }
+}
